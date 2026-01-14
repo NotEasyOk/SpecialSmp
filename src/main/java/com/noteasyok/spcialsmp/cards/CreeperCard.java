2d6080ac99e185ed0c8323fc82d60a661ed22a5d
NotEasyOk <masterplayz755@gmail.com>
2026-01-14 10:15:16 +0530

    Update CreeperCard.java

--- src/main/java/com/noteasyok/spcialsmp/cards/CreeperCard.java
+++ src/main/java/com/noteasyok/spcialsmp/cards/CreeperCard.java
@@ -1,1 +1,29 @@
+package com.noteasyok.spcialsmp.cards;
 
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+
+public class CreeperCard implements Card {
+
+    @Override
+    public String getName() {
+        return "Creeper";
+    }
+
+    @Override
+    public void leftClick(Player p) {
+        Location l = p.getLocation();
+        l.getWorld().createExplosion(l, 4f, false, false);
+    }
+
+    @Override
+    public void rightClick(Player p) {
+        Location l = p.getTargetBlockExact(50).getLocation();
+        l.getWorld().createExplosion(l, 6f, false, false);
+    }
+
+    @Override
+    public void shiftRightClick(Player p) {
+        p.getWorld().createExplosion(p.getLocation(), 5f, false, false);
+    }
+}
