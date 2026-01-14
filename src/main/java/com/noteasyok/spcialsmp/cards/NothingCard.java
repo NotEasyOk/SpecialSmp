871619feb8760f84037f4cff9d9c5b59c61b8f97
NotEasyOk <masterplayz755@gmail.com>
2026-01-14 10:10:16 +0530

    Update NothingCard.java

--- src/main/java/com/noteasyok/spcialsmp/cards/NothingCard.java
+++ src/main/java/com/noteasyok/spcialsmp/cards/NothingCard.java
@@ -1,1 +1,18 @@
+package com.noteasyok.spcialsmp.cards;
 
+import org.bukkit.entity.Player;
+
+public class NothingCard implements Card {
+
+    public String getName() { return "Nothing"; }
+
+    public void leftClick(Player p) {
+        p.getWorld().setTime(p.getWorld().getTime() > 12000 ? 0 : 13000);
+    }
+
+    public void rightClick(Player p) {}
+
+    public void shiftRightClick(Player p) {
+        p.setFallDistance(0);
+    }
+}
