c2cb0ab97e267616238a0064017d51ff745ee1b7
NotEasyOk <masterplayz755@gmail.com>
2026-01-14 16:59:54 +0530

    Update ZombieCard.java

--- src/main/java/com/noteasyok/spcialsmp/cards/ZombieCard.java
+++ src/main/java/com/noteasyok/spcialsmp/cards/ZombieCard.java
@@ -1,25 +1,40 @@
 package com.noteasyok.spcialsmp.cards;
 
+import com.noteasyok.spcialsmp.SpcialSmp;
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Zombie;
+import org.bukkit.event.entity.EntityTargetEvent;
 import org.bukkit.inventory.EntityEquipment;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.Material;
 
+import java.util.*;
+
 public class ZombieCard implements Card {
 
-    @Override
-    public String getName() {
-        return "Zombie";
-    }
+    private final Map<UUID, Integer> spawnCount = new HashMap<>();
 
     @Override
-    public void leftClick(Player p) {
-        Zombie z = p.getWorld().spawn(p.getLocation(), Zombie.class);
-        z.setBaby(true);
+    public void leftClick(Player player) {
 
-        EntityEquipment eq = z.getEquipment();
+        UUID id = player.getUniqueId();
+        int max = SpcialSmp.get().getConfig().getInt("zombie-card.max-zombies", 2);
+
+        int used = spawnCount.getOrDefault(id, 0);
+        if (used >= max) {
+            player.sendMessage("Zombie limit reached");
+            return;
+        }
+
+        Zombie zombie = player.getWorld().spawn(player.getLocation(), Zombie.class);
+        zombie.setBaby(true);
+        zombie.setCustomName(player.getName() + "'s Zombie");
+        zombie.setCustomNameVisible(false);
+        zombie.setTarget(null);
+
+        EntityEquipment eq = zombie.getEquipment();
         if (eq != null) {
             eq.setHelmet(new ItemStack(Material.NETHERITE_HELMET));
             eq.setChestplate(new ItemStack(Material.NETHERITE_CHESTPLATE));
@@ -27,17 +42,21 @@ public class ZombieCard implements Card {
             eq.setBoots(new ItemStack(Material.NETHERITE_BOOTS));
         }
 
-        Bukkit.getScheduler().runTaskLater(
-                Bukkit.getPluginManager().getPlugin("spcialSmp"),
-                z::remove, 200
-        );
+        zombie.setMetadata("owner", new org.bukkit.metadata.FixedMetadataValue(
+                SpcialSmp.get(), player.getUniqueId().toString()
+        ));
+
+        spawnCount.put(id, used + 1);
+
+        Bukkit.getScheduler().runTaskLater(SpcialSmp.get(), () -> {
+            zombie.remove();
+            spawnCount.put(id, spawnCount.get(id) - 1);
+        }, 20L * 10);
     }
 
     @Override
-    public void rightClick(Player p) {}
+    public void rightClick(Player player) {}
 
     @Override
-    public void shiftRightClick(Player p) {
-        p.sendMessage("You turned into a zombie (visual only)");
-    }
-              }
+    public void shiftRightClick(Player player) {}
+}
