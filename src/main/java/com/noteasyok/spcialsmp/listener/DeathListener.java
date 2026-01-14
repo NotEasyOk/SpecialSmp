2c6a653230a8aaf96bf734c0f370d9dd627db97e
NotEasyOk <masterplayz755@gmail.com>
2026-01-14 10:24:42 +0530

    Update DeathListener.java

--- src/main/java/com/noteasyok/spcialsmp/listener/DeathListener.java
+++ src/main/java/com/noteasyok/spcialsmp/listener/DeathListener.java
@@ -1,1 +1,21 @@
+package com.noteasyok.spcialsmp.listener;
 
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
+
+public class DeathListener implements Listener {
+
+    @EventHandler
+    public void onDeath(PlayerDeathEvent e) {
+
+        // Natural death -> NO DROP
+        if (e.getEntity().getKiller() == null) {
+            e.getDrops().clear();
+            return;
+        }
+
+        // Player kill -> allow drop (keep items)
+        // Tum yahan custom logic bhi add kar sakte ho
+    }
+}
