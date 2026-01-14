2a3a20442933504155f37517fafc9293cff22897
NotEasyOk <masterplayz755@gmail.com>
2026-01-14 10:09:26 +0530

    Update HerobrineCard.java

--- src/main/java/com/noteasyok/spcialsmp/cards/HerobrineCard.java
+++ src/main/java/com/noteasyok/spcialsmp/cards/HerobrineCard.java
@@ -1,1 +1,27 @@
+package com.noteasyok.spcialsmp.cards;
 
+import org.bukkit.World;
+import org.bukkit.entity.Player;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+
+public class HerobrineCard implements Card {
+
+    public String getName() { return "Herobrine"; }
+
+    public void leftClick(Player p) {
+        World w = p.getWorld();
+        for (int i = 0; i < 5; i++)
+            w.strikeLightning(p.getLocation());
+    }
+
+    public void rightClick(Player p) {
+        p.addPotionEffect(new PotionEffect(PotionEffectType.DARKNESS, 200, 1));
+        p.setAllowFlight(true);
+    }
+
+    public void shiftRightClick(Player p) {
+        p.addPotionEffect(new PotionEffect(PotionEffectType.STRENGTH, 200, 3));
+        p.addPotionEffect(new PotionEffect(PotionEffectType.REGENERATION, 200, 3));
+    }
+}
