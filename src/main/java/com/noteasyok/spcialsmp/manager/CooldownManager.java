bc449529bcd811f950c469f2ca36f8a6de11cf02
NotEasyOk <masterplayz755@gmail.com>
2026-01-14 10:06:23 +0530

    Update CooldownManager.java

--- src/main/java/com/noteasyok/spcialsmp/manager/CooldownManager.java
+++ src/main/java/com/noteasyok/spcialsmp/manager/CooldownManager.java
@@ -1,1 +1,23 @@
+package com.noteasyok.spcialsmp.manager;
 
+import java.util.HashMap;
+import java.util.UUID;
+
+public class CooldownManager {
+
+    private final HashMap<String, Long> map = new HashMap<>();
+
+    public boolean onCooldown(UUID uuid, String key) {
+        String k = uuid + ":" + key;
+        if (!map.containsKey(k)) return false;
+        return map.get(k) > System.currentTimeMillis();
+    }
+
+    public long timeLeft(UUID uuid, String key) {
+        return (map.get(uuid + ":" + key) - System.currentTimeMillis()) / 1000;
+    }
+
+    public void set(UUID uuid, String key, int sec) {
+        map.put(uuid + ":" + key, System.currentTimeMillis() + sec * 1000L);
+    }
+}
