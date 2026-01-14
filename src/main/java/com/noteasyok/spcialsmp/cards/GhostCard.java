d4dfdd83605f9471fd33eaa7b735c30c3b5a2f44
NotEasyOk <masterplayz755@gmail.com>
2026-01-14 10:16:43 +0530

    Update GhostCard.java

--- src/main/java/com/noteasyok/spcialsmp/cards/GhostCard.java
+++ src/main/java/com/noteasyok/spcialsmp/cards/GhostCard.java
@@ -1,1 +1,29 @@
+package com.noteasyok.spcialsmp.cards;
 
+import org.bukkit.entity.Player;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+
+public class GhostCard implements Card {
+
+    @Override
+    public String getName() {
+        return "Ghost";
+    }
+
+    @Override
+    public void leftClick(Player p) {
+        p.addPotionEffect(new PotionEffect(PotionEffectType.INVISIBILITY, 200, 0));
+    }
+
+    @Override
+    public void rightClick(Player p) {
+        p.setAllowFlight(true);
+        p.setFlying(true);
+    }
+
+    @Override
+    public void shiftRightClick(Player p) {
+        p.addPotionEffect(new PotionEffect(PotionEffectType.INVISIBILITY, 1200, 0));
+    }
+}
