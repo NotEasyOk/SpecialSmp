5974723cccc7c986b7dc02709d430e76fc5b518f
NotEasyOk <masterplayz755@gmail.com>
2026-01-14 10:16:02 +0530

    Update LightingCard.java

--- src/main/java/com/noteasyok/spcialsmp/cards/LightingCard.java
+++ src/main/java/com/noteasyok/spcialsmp/cards/LightingCard.java
@@ -1,1 +1,27 @@
+package com.noteasyok.spcialsmp.cards;
 
+import org.bukkit.entity.Player;
+
+public class LightingCard implements Card {
+
+    @Override
+    public String getName() {
+        return "Lighting";
+    }
+
+    @Override
+    public void leftClick(Player p) {
+        p.getWorld().strikeLightning(p.getLocation());
+    }
+
+    @Override
+    public void rightClick(Player p) {
+        boolean storm = p.getWorld().hasStorm();
+        p.getWorld().setStorm(!storm);
+    }
+
+    @Override
+    public void shiftRightClick(Player p) {
+        p.sendMessage("Lightning trail enabled (no damage)");
+    }
+}
