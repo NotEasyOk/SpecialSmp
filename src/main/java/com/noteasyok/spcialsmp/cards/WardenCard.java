e951c8a0ed23c08613ce0dba4d9a30e550a695cf
NotEasyOk <masterplayz755@gmail.com>
2026-01-14 10:14:12 +0530

    Update WardenCard.java

--- src/main/java/com/noteasyok/spcialsmp/cards/WardenCard.java
+++ src/main/java/com/noteasyok/spcialsmp/cards/WardenCard.java
@@ -1,1 +1,28 @@
+package com.noteasyok.spcialsmp.cards;
 
+import org.bukkit.entity.Player;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+
+public class WardenCard implements Card {
+
+    @Override
+    public String getName() {
+        return "Warden";
+    }
+
+    @Override
+    public void leftClick(Player p) {
+        p.addPotionEffect(new PotionEffect(PotionEffectType.DARKNESS, 200, 1));
+    }
+
+    @Override
+    public void rightClick(Player p) {
+        p.addPotionEffect(new PotionEffect(PotionEffectType.HEALTH_BOOST, 300, 4));
+    }
+
+    @Override
+    public void shiftRightClick(Player p) {
+        p.sendMessage("Magnet power activated (logic basic)");
+    }
+}
