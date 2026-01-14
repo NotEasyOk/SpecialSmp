5ca506dea1e91ac62914c3a02351d41ad8bc5df7
NotEasyOk <masterplayz755@gmail.com>
2026-01-14 10:17:51 +0530

    Update UnlimitedCard.java

--- src/main/java/com/noteasyok/spcialsmp/cards/UnlimitedCard.java
+++ src/main/java/com/noteasyok/spcialsmp/cards/UnlimitedCard.java
@@ -1,1 +1,38 @@
+package com.noteasyok.spcialsmp.cards;
 
+import org.bukkit.entity.Player;
+
+public class UnlimitedCard implements Card {
+
+    private final Card[] cards = {
+            new EndermanCard(),
+            new HerobrineCard(),
+            new ZombieCard(),
+            new WardenCard(),
+            new CreeperCard(),
+            new LightingCard(),
+            new GhostCard(),
+            new RuinCard(),
+            new NothingCard()
+    };
+
+    @Override
+    public String getName() {
+        return "Unlimited";
+    }
+
+    @Override
+    public void leftClick(Player p) {
+        for (Card c : cards) c.leftClick(p);
+    }
+
+    @Override
+    public void rightClick(Player p) {
+        for (Card c : cards) c.rightClick(p);
+    }
+
+    @Override
+    public void shiftRightClick(Player p) {
+        for (Card c : cards) c.shiftRightClick(p);
+    }
+}
