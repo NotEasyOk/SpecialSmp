9fa0047b35341dcf94a9a68a2766d23f400177c3
NotEasyOk <masterplayz755@gmail.com>
2026-01-14 10:04:40 +0530

    Update Card.java

--- src/main/java/com/noteasyok/spcialsmp/cards/Card.java
+++ src/main/java/com/noteasyok/spcialsmp/cards/Card.java
@@ -1,1 +1,10 @@
+package com.noteasyok.spcialsmp.cards;
 
+import org.bukkit.entity.Player;
+
+public interface Card {
+    String getName();
+    void leftClick(Player p);
+    void rightClick(Player p);
+    void shiftRightClick(Player p);
+}
