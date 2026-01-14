88ef80d5ddf66a9e1e55295b29cb1a817d34049c
NotEasyOk <masterplayz755@gmail.com>
2026-01-14 11:57:18 +0530

    Update RuinCard.java

--- src/main/java/com/noteasyok/spcialsmp/cards/RuinCard.java
+++ src/main/java/com/noteasyok/spcialsmp/cards/RuinCard.java
@@ -1,1 +1,29 @@
+package com.noteasyok.spcialsmp.cards;
 
+import org.bukkit.entity.Player;
+import org.bukkit.entity.Silverfish;
+
+public class RuinCard implements Card {
+
+    @Override
+    public String getName() {
+        return "Ruin";
+    }
+
+    @Override
+    public void leftClick(Player p) {
+        p.sendMessage("Ruin area activated");
+    }
+
+    @Override
+    public void rightClick(Player p) {
+        for (int i = 0; i < 10; i++) {
+            p.getWorld().spawn(p.getLocation(), Silverfish.class);
+        }
+    }
+
+    @Override
+    public void shiftRightClick(Player p) {
+        p.sendMessage("Enemies poisoned");
+    }
+              }
