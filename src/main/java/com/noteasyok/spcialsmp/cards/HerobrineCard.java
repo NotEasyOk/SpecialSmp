cf0319ffa2a7ace63df71fa4b32538cf21a7cb79
NotEasyOk <masterplayz755@gmail.com>
2026-01-15 18:24:44 +0530

    Update HerobrineCard.java

--- src/main/java/com/noteasyok/spcialsmp/cards/HerobrineCard.java
+++ src/main/java/com/noteasyok/spcialsmp/cards/HerobrineCard.java
@@ -1,5 +1,6 @@
 package com.noteasyok.spcialsmp.cards;
 
+import com.noteasyok.spcialsmp.SpcialSmp;
 import org.bukkit.World;
 import org.bukkit.entity.Player;
 import org.bukkit.potion.PotionEffect;
