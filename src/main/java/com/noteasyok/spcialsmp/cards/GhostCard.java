72470ec734d5bd1e0ed1284bd499fb42675db3a5
NotEasyOk <masterplayz755@gmail.com>
2026-01-15 17:49:20 +0530

    Update GhostCard.java

--- src/main/java/com/noteasyok/spcialsmp/cards/GhostCard.java
+++ src/main/java/com/noteasyok/spcialsmp/cards/GhostCard.java
@@ -1,6 +1,6 @@
 package com.noteasyok.spcialsmp.cards;
 
-import com.noteasyok.spcialsmp.SpicialSmp;
+import com.noteasyok.spcialsmp.SpcialSmp;
 import org.bukkit.Bukkit;
 import org.bukkit.GameMode;
 import org.bukkit.entity.Player;
