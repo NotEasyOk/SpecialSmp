bbed388a609b06d4805518ce2e747fb8715eac83
NotEasyOk <masterplayz755@gmail.com>
2026-01-15 18:27:59 +0530

    Update RuinCard.java

--- src/main/java/com/noteasyok/spcialsmp/cards/RuinCard.java
+++ src/main/java/com/noteasyok/spcialsmp/cards/RuinCard.java
@@ -1,5 +1,6 @@
 package com.noteasyok.spcialsmp.cards;
 
+import com.noteasyok.spcialsmp.SpcialSmp;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Silverfish;
 import org.bukkit.potion.PotionEffect;
