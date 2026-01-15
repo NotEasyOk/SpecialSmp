10e5d616a381a4f16d1bf5b5689bc0dbce90e06c
NotEasyOk <masterplayz755@gmail.com>
2026-01-15 08:04:06 +0530

    Update GhostCard.java

--- src/main/java/com/noteasyok/spcialsmp/cards/GhostCard.java
+++ src/main/java/com/noteasyok/spcialsmp/cards/GhostCard.java
@@ -1,5 +1,7 @@
 package com.noteasyok.spcialsmp.cards;
 
+import com.noteasyok.spcialsmp.SpcialSmp;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
@@ -8,22 +10,29 @@ public class GhostCard implements Card {
 
     @Override
     public String getName() {
-        return "Ghost";
+        return "Ghost Card";
     }
 
     @Override
     public void leftClick(Player p) {
-        p.addPotionEffect(new PotionEffect(PotionEffectType.INVISIBILITY, 200, 0));
+        p.addPotionEffect(new PotionEffect(
+                PotionEffectType.SLOW_FALLING, 200, 1));
     }
 
     @Override
     public void rightClick(Player p) {
         p.setAllowFlight(true);
         p.setFlying(true);
+
+        Bukkit.getScheduler().runTaskLater(SpcialSmp.get(), () -> {
+            p.setFlying(false);
+            p.setAllowFlight(false);
+        }, 20L * 40);
     }
 
     @Override
     public void shiftRightClick(Player p) {
-        p.addPotionEffect(new PotionEffect(PotionEffectType.INVISIBILITY, 1200, 0));
+        p.addPotionEffect(new PotionEffect(
+                PotionEffectType.INVISIBILITY, 1200, 0));
     }
 }
