c6c64d232a6f2e0703f74ab9d0f76c1177a0a435
NotEasyOk <masterplayz755@gmail.com>
2026-01-16 16:21:48 +0530

    Update HerobrineCard.java

--- src/main/java/com/noteasyok/spcialsmp/cards/HerobrineCard.java
+++ src/main/java/com/noteasyok/spcialsmp/cards/HerobrineCard.java
@@ -6,7 +6,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
-public class HerobrineCard implements BaseCard {
+public class HerobrineCard extends BaseCard {
 
     public String getName() { return "Herobrine Card"; }
 
