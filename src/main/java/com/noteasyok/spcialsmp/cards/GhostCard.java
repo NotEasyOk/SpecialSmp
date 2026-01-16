1738b5c0f1fb94b057daa3658275a814ef57d45f
NotEasyOk <masterplayz755@gmail.com>
2026-01-16 14:28:57 +0530

    Update GhostCard.java

--- src/main/java/com/noteasyok/spcialsmp/cards/GhostCard.java
+++ src/main/java/com/noteasyok/spcialsmp/cards/GhostCard.java
@@ -7,7 +7,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
-public class GhostCard implements Card {
+public class GhostCard implements BaseCard {
 
     @Override
     public String getName() {
