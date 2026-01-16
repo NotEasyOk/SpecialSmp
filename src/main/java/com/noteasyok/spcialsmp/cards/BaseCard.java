b962e07c891932c2a72196387b2b229e816fa900
NotEasyOk <masterplayz755@gmail.com>
2026-01-16 14:25:53 +0530

    Create BaseCard.java

---
+++ src/main/java/com/noteasyok/spcialsmp/cards/BaseCard.java
@@ -0,0 +1,18 @@
+package com.noteasyok.spcialsmp.cards;
+
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+public abstract class BaseCard implements Card {
+
+    @Override
+    public ItemStack createItem() {
+        ItemStack item = new ItemStack(Material.NETHER_STAR);
+        ItemMeta meta = item.getItemMeta();
+        meta.setDisplayName(getName());
+        item.setItemMeta(meta);
+        return item;
+    }
+}
+
