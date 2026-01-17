a4f54ad072c76e4081700238d003a4fd6db6b0cb
NotEasyOk <masterplayz755@gmail.com>
2026-01-17 22:19:32 +0530

    Update UltimateHoldListener.java

--- src/main/java/com/noteasyok/spcialsmp/listener/UltimateHoldListener.java
+++ src/main/java/com/noteasyok/spcialsmp/listener/UltimateHoldListener.java
@@ -15,7 +15,32 @@ import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 import org.bukkit.scheduler.BukkitRunnable;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class UltimateHoldListener implements Listener {
+
+    private static final Set<PotionEffectType> ULTIMATE_EFFECTS = new HashSet<>();
+
+    static {
+        ULTIMATE_EFFECTS.add(PotionEffectType.SPEED);
+        ULTIMATE_EFFECTS.add(PotionEffectType.HASTE);
+        ULTIMATE_EFFECTS.add(PotionEffectType.STRENGTH);
+        ULTIMATE_EFFECTS.add(PotionEffectType.JUMP_BOOST);
+        ULTIMATE_EFFECTS.add(PotionEffectType.REGENERATION);
+        ULTIMATE_EFFECTS.add(PotionEffectType.RESISTANCE);
+        ULTIMATE_EFFECTS.add(PotionEffectType.FIRE_RESISTANCE);
+        ULTIMATE_EFFECTS.add(PotionEffectType.WATER_BREATHING);
+        ULTIMATE_EFFECTS.add(PotionEffectType.NIGHT_VISION);
+        ULTIMATE_EFFECTS.add(PotionEffectType.ABSORPTION);
+        ULTIMATE_EFFECTS.add(PotionEffectType.HEALTH_BOOST);
+        ULTIMATE_EFFECTS.add(PotionEffectType.SATURATION);
+        ULTIMATE_EFFECTS.add(PotionEffectType.LUCK);
+        ULTIMATE_EFFECTS.add(PotionEffectType.CONDUIT_POWER);
+        ULTIMATE_EFFECTS.add(PotionEffectType.DOLPHINS_GRACE);
+        // ✅ Poison effect add kiya gaya hai
+        ULTIMATE_EFFECTS.add(PotionEffectType.POISON); 
+    }
 
     public UltimateHoldListener() {
         new BukkitRunnable() {
@@ -23,12 +48,12 @@ public class UltimateHoldListener implements Listener {
             public void run() {
                 for (Player p : Bukkit.getOnlinePlayers()) {
                     if (isUltimate(p.getInventory().getItemInMainHand())) {
-                        // All effects
-                        p.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 40, 1, false, false));
-                        p.addPotionEffect(new PotionEffect(PotionEffectType.RESISTANCE, 40, 1, false, false));
-                        p.addPotionEffect(new PotionEffect(PotionEffectType.STRENGTH, 40, 1, false, false));
+                        // Sare effects apply karo
+                        for (PotionEffectType type : ULTIMATE_EFFECTS) {
+                            p.addPotionEffect(new PotionEffect(type, 60, 1, false, false, true));
+                        }
                         
-                        // Start Orbit
+                        // Orbit start karo agar nahi chal raha
                         BaseCard card = CardRegistry.getCards().get("Ultimate Card");
                         if (card instanceof UltimateCard uc) {
                             uc.startOrbit(p);
@@ -41,8 +66,7 @@ public class UltimateHoldListener implements Listener {
 
     @EventHandler
     public void onSwap(PlayerItemHeldEvent e) {
-        ItemStack item = e.getPlayer().getInventory().getItem(e.getNewSlot());
-        if (isUltimate(item)) {
+        if (isUltimate(e.getPlayer().getInventory().getItem(e.getNewSlot()))) {
             BaseCard card = CardRegistry.getCards().get("Ultimate Card");
             if (card instanceof UltimateCard uc) uc.startOrbit(e.getPlayer());
         }
@@ -57,4 +81,4 @@ public class UltimateHoldListener implements Listener {
         String name = ChatColor.stripColor(item.getItemMeta().getDisplayName());
         return name.equals("Ultimate Card");
     }
-    }
+                                }
