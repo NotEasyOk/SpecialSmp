1a2cdf9ddaeea2371a155bfaeaf17bc64c4feb5a
NotEasyOk <masterplayz755@gmail.com>
2026-01-19 20:42:23 +0530

    Update WardenCard.java

--- src/main/java/com/noteasyok/spcialsmp/cards/WardenCard.java
+++ src/main/java/com/noteasyok/spcialsmp/cards/WardenCard.java
@@ -29,9 +29,9 @@ public class WardenCard extends BaseCard {
     /* ---------------- LEFT CLICK (Health Boost) ---------------- */
     @Override
     public void leftClick(Player p) {
-        // Config path: cards.warden.left_click_cooldown
         int cooldownSec = SpcialSmp.get().getConfig().getInt("cards.warden.left_click_cooldown", 30);
-        if (!checkCooldown(p, "left", cooldownSec)) return;
+        // FIXED: checkCooldown ki jagah isCool use kiya
+        if (!isCool(p, "left", cooldownSec)) return;
 
         double baseMax = p.getAttribute(Attribute.GENERIC_MAX_HEALTH).getBaseValue();
         p.getAttribute(Attribute.GENERIC_MAX_HEALTH).setBaseValue(100.0);
@@ -53,9 +53,9 @@ public class WardenCard extends BaseCard {
     /* ---------------- RIGHT CLICK (SONIC BOOM) ---------------- */
     @Override
     public void rightClick(Player p) {
-        // Config path: cards.warden.right_click_cooldown
         int cooldownSec = SpcialSmp.get().getConfig().getInt("cards.warden.right_click_cooldown", 10);
-        if (!checkCooldown(p, "right", cooldownSec)) return;
+        // FIXED: checkCooldown ki jagah isCool use kiya
+        if (!isCool(p, "right", cooldownSec)) return;
 
         Location start = p.getEyeLocation();
         Vector dir = start.getDirection().normalize();
@@ -71,15 +71,15 @@ public class WardenCard extends BaseCard {
                 }
             }
         }
-        p.getWorld().playSound(p.getLocation(), "entity.warden.sonic_boom", 3f, 1f);
+        p.getWorld().playSound(p.getLocation(), org.bukkit.Sound.ENTITY_WARDEN_SONIC_BOOM, 3f, 1f);
     }
 
     /* ---------------- SHIFT + RIGHT (Buffs) ---------------- */
     @Override
     public void shiftRightClick(Player p) {
-        // Config path: cards.warden.shift_right_click_cooldown
         int cooldownSec = SpcialSmp.get().getConfig().getInt("cards.warden.shift_click_cooldown", 45);
-        if (!checkCooldown(p, "shift", cooldownSec)) return;
+        // FIXED: checkCooldown ki jagah isCool use kiya
+        if (!isCool(p, "shift", cooldownSec)) return;
 
         p.addPotionEffect(new PotionEffect(PotionEffectType.STRENGTH, 20 * 15, 3));
         p.addPotionEffect(new PotionEffect(PotionEffectType.RESISTANCE, 20 * 15, 3));
@@ -88,22 +88,19 @@ public class WardenCard extends BaseCard {
 
     // --- COOLDOWN HELPER ---
     private boolean isCool(Player p, String key, int seconds) {
-    if (seconds <= 0) return true;
-    long now = System.currentTimeMillis();
-    
-    // Map ki key String honi chahiye
-    String mapKey = p.getUniqueId().toString() + "_" + key;
-    
-    if (cooldowns.containsKey(mapKey)) {
-        long timeLeft = (cooldowns.get(mapKey) - now) / 1000;
-        if (timeLeft > 0) {
-            // Config se message uthayega
-            String rawMsg = SpcialSmp.get().getConfig().getString("messages.cooldown-active", "§cWait %time%s");
-            p.sendMessage(rawMsg.replace("%time%", String.valueOf(timeLeft)));
-            return false;
+        if (seconds <= 0) return true;
+        long now = System.currentTimeMillis();
+        String mapKey = p.getUniqueId().toString() + "_" + key;
+        
+        if (cooldowns.containsKey(mapKey)) {
+            long timeLeft = (cooldowns.get(mapKey) - now) / 1000;
+            if (timeLeft > 0) {
+                String rawMsg = SpcialSmp.get().getConfig().getString("messages.cooldown-active", "§cWait %time%s");
+                p.sendMessage(rawMsg.replace("%time%", String.valueOf(timeLeft)));
+                return false;
+            }
         }
+        cooldowns.put(mapKey, now + (seconds * 1000L));
+        return true;
     }
-    cooldowns.put(mapKey, now + (seconds * 1000L));
-    return true;
-   }
-}
+                    }
