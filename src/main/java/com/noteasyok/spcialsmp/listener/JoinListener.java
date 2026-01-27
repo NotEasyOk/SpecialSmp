e68f6878ab77b71ce877012b65f02aae242c81a5
NotEasyOk <masterplayz755@gmail.com>
2026-01-27 18:31:59 +0530

    Update JoinListener.java

--- src/main/java/com/noteasyok/spcialsmp/listener/JoinListener.java
+++ src/main/java/com/noteasyok/spcialsmp/listener/JoinListener.java
@@ -2,6 +2,7 @@ package com.noteasyok.spcialsmp.listener;
 
 import com.noteasyok.spcialsmp.SpcialSmp;
 import com.noteasyok.spcialsmp.manager.CardSpinner;
+import com.noteasyok.spcialsmp.manager.FuelManager; // Naya import
 import org.bukkit.attribute.Attribute;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -13,6 +14,13 @@ public class JoinListener implements Listener {
     public void onJoin(PlayerJoinEvent e) {
         Player p = e.getPlayer();
         var dataManager = SpcialSmp.get().getPlayerDataManager();
+
+        // --- FUEL SYSTEM START ---
+        // Agar naya player hai to use 24 hours fuel do
+        if (!p.hasPlayedBefore()) {
+            FuelManager.setFuel(p, 1440);
+        }
+        // --- FUEL SYSTEM END ---
 
         if (p.getAttribute(Attribute.GENERIC_SCALE) != null) 
             p.getAttribute(Attribute.GENERIC_SCALE).setBaseValue(1.0);
