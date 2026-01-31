5929d971d930bc3c6d18a72da1c35098dcbfd76e
NotEasyOk <masterplayz755@gmail.com>
2026-01-31 09:12:38 +0530

    Update LightingCard.java

--- src/main/java/com/noteasyok/spcialsmp/cards/LightingCard.java
+++ src/main/java/com/noteasyok/spcialsmp/cards/LightingCard.java
@@ -5,6 +5,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
+import org.bukkit.Material;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 import org.bukkit.util.RayTraceResult;
