cc390bf210c7e5dc23c5bab871cdb59200af1df0
NotEasyOk <masterplayz755@gmail.com>
2026-01-31 09:12:05 +0530

    Update HerobrineCard.java

--- src/main/java/com/noteasyok/spcialsmp/cards/HerobrineCard.java
+++ src/main/java/com/noteasyok/spcialsmp/cards/HerobrineCard.java
@@ -5,6 +5,7 @@ import org.bukkit.ChatColor;
 import org.bukkit.GameMode;
 import org.bukkit.World;
 import org.bukkit.attribute.Attribute;
+import org.bukkit.Material;
 import org.bukkit.attribute.AttributeInstance;
 import org.bukkit.entity.Player;
 import org.bukkit.potion.PotionEffect;
