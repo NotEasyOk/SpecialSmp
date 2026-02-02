45d57544bb5670e875a030678effa623c7901e02
NotEasyOk <masterplayz755@gmail.com>
2026-02-02 11:06:28 +0530

    Update RuinWorldGenerator.java

--- src/main/java/com/noteasyok/spcialsmp/cards/RuinWorldGenerator.java
+++ src/main/java/com/noteasyok/spcialsmp/cards/RuinWorldGenerator.java
@@ -7,7 +7,7 @@ import org.bukkit.generator.BlockPopulator;
 import org.bukkit.generator.ChunkGenerator;
 import org.bukkit.util.noise.SimplexOctaveGenerator;
 
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 
@@ -17,31 +17,27 @@ public class RuinWorldGenerator extends ChunkGenerator {
     public ChunkData generateChunkData(World world, Random random, int chunkX, int chunkZ, BiomeGrid biome) {
         ChunkData chunk = createChunkData(world);
         SimplexOctaveGenerator generator = new SimplexOctaveGenerator(new Random(world.getSeed()), 8);
-        generator.setScale(0.005); // Terrain Scale (Jitna kam, utna bada pahad)
+        generator.setScale(0.005); 
 
         for (int x = 0; x < 16; x++) {
             for (int z = 0; z < 16; z++) {
                 int realX = chunkX * 16 + x;
                 int realZ = chunkZ * 16 + z;
 
-                // Strange Terrain Height
                 double noise = generator.noise(realX, realZ, 0.5, 0.5) * 15;
                 int currentHeight = (int) (65 + noise);
 
                 for (int y = 0; y < currentHeight; y++) {
                     if (y == currentHeight - 1) {
-                        // TOP LAYER: Ajeeb Blocks
                         int r = random.nextInt(10);
-                        if (r < 5) chunk.setBlock(x, y, z, Material.SCULK); // Dark Blue Weird Block
-                        else if (r < 8) chunk.setBlock(x, y, z, Material.MOSS_BLOCK); // Green Moss
-                        else chunk.setBlock(x, y, z, Material.SOUL_SAND); // Creepy Sand
+                        if (r < 5) chunk.setBlock(x, y, z, Material.SCULK);
+                        else if (r < 8) chunk.setBlock(x, y, z, Material.MOSS_BLOCK);
+                        else chunk.setBlock(x, y, z, Material.SOUL_SAND);
                     } else {
-                        // UNDERGROUND
                         chunk.setBlock(x, y, z, Material.BLACKSTONE);
                     }
                 }
                 
-                // Set Biome to SWAMP (Taaki paani ganda/green dikhe)
                 for (int y = 0; y < 100; y++) {
                     biome.setBiome(x, y, z, Biome.SWAMP); 
                 }
@@ -50,19 +46,31 @@ public class RuinWorldGenerator extends ChunkGenerator {
         return chunk;
     }
 
-    // --- CUSTOM TREE GENERATOR (Poison Trees) ---
     @Override
     public List<BlockPopulator> getDefaultPopulators(World world) {
-        return Collections.singletonList(new BlockPopulator() {
+        return Arrays.asList(new BlockPopulator() {
             @Override
             public void populate(World world, Random random, org.bukkit.Chunk chunk) {
-                if (random.nextInt(10) < 3) { // 30% chance per chunk
-                    int x = random.nextInt(15);
-                    int z = random.nextInt(15);
-                    int y = world.getHighestBlockYAt(chunk.getX() * 16 + x, chunk.getZ() * 16 + z);
-                    
-                    if (world.getBlockAt(chunk.getX() * 16 + x, y, chunk.getZ() * 16 + z).getType() != Material.WATER) {
-                        createPoisonTree(world, chunk.getX() * 16 + x, y, chunk.getZ() * 16 + z, random);
+                int cx = chunk.getX() * 16;
+                int cz = chunk.getZ() * 16;
+
+                // 1. Poison Trees Generator (30% Chance)
+                if (random.nextInt(100) < 30) {
+                    int x = cx + random.nextInt(15);
+                    int z = cz + random.nextInt(15);
+                    int y = world.getHighestBlockYAt(x, z);
+                    if (world.getBlockAt(x, y, z).getType() != Material.WATER) {
+                        createPoisonTree(world, x, y, z, random);
+                    }
+                }
+
+                // 2. Corrupted Houses Generator (5% Chance - Rare structures)
+                if (random.nextInt(100) < 5) {
+                    int x = cx + random.nextInt(15);
+                    int z = cz + random.nextInt(15);
+                    int y = world.getHighestBlockYAt(x, z);
+                    if (y > 60) {
+                        createCorruptedHouse(world, x, y, z, random);
                     }
                 }
             }
@@ -70,12 +78,9 @@ public class RuinWorldGenerator extends ChunkGenerator {
     }
 
     private void createPoisonTree(World world, int x, int y, int z, Random random) {
-        // Strange Tree Stem (Warped Stem - Green/Blue Wood)
         for (int i = 0; i < 5 + random.nextInt(3); i++) {
             world.getBlockAt(x, y + i, z).setType(Material.WARPED_STEM);
         }
-        
-        // Strange Leaves (Nether Wart & Shroomlight)
         int top = y + 4;
         for (int lx = x - 2; lx <= x + 2; lx++) {
             for (int lz = z - 2; lz <= z + 2; lz++) {
@@ -84,11 +89,34 @@ public class RuinWorldGenerator extends ChunkGenerator {
                         if (random.nextBoolean()) {
                             world.getBlockAt(lx, ly, lz).setType(Material.WARPED_WART_BLOCK);
                         } else if (random.nextInt(10) == 0) {
-                            world.getBlockAt(lx, ly, lz).setType(Material.SHROOMLIGHT); // Glow wala block
+                            world.getBlockAt(lx, ly, lz).setType(Material.SHROOMLIGHT);
                         }
                     }
                 }
             }
         }
     }
-                          }
+
+    private void createCorruptedHouse(World world, int x, int y, int z, Random random) {
+        int width = 5 + random.nextInt(3);
+        int height = 4 + random.nextInt(2);
+        int depth = 5 + random.nextInt(3);
+
+        for (int bx = x; bx < x + width; bx++) {
+            for (int bz = z; bz < z + depth; bz++) {
+                for (int by = y; by < y + height; by++) {
+                    // Tute huye ghar ka logic (70% blocks place honge, 30% air taaki broken dikhe)
+                    if (random.nextInt(10) < 7) {
+                        // Deewar aur chhat
+                        if (by == y || by == y + height - 1 || bx == x || bx == x + width - 1 || bz == z || bz == z + depth - 1) {
+                            Material mat = (random.nextBoolean()) ? Material.POLISHED_BLACKSTONE_BRICKS : Material.SCULK;
+                            world.getBlockAt(bx, by, bz).setType(mat);
+                        }
+                    }
+                }
+            }
+        }
+        // Loot ka ajeeb dabba (Optional)
+        world.getBlockAt(x + 1, y + 1, z + 1).setType(Material.REINFORCED_DEEPSLATE);
+    }
+                        }
