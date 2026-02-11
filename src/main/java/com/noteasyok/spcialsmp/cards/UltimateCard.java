791e6c4b13ba3350925df3e578085f5331938906
NotEasyOk <masterplayz755@gmail.com>
2026-02-11 11:45:44 +0530

    Update UltimateCard.java

--- src/main/java/com/noteasyok/spcialsmp/cards/UltimateCard.java
+++ src/main/java/com/noteasyok/spcialsmp/cards/UltimateCard.java
@@ -310,7 +310,7 @@ if (timer % 15 == 0) {
                     // Transformation: 6x Size + Handle UP (Pointing Down)
                     Transformation tr = sword.getTransformation();
                     tr.getScale().set(6.5f, 6.5f, 6.5f);
-                    tr.getLeftRotation().set(new AxisAngle4f(0.0f, 0.0f, 0.0f, 1.0f));
+                    tr.getLeftRotation().set(new AxisAngle4f((float) Math.PI,1.0f, 0.0f, 0.0f,));
                     sword.setTransformation(tr);
 
                     // Falling & Explosion Logic
