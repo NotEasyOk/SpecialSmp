609a793e607a5969af5e495fc786ffd543485931
NotEasyOk <masterplayz755@gmail.com>
2026-02-13 14:52:55 +0530

    Update ZombieCard.java

--- src/main/java/com/noteasyok/spcialsmp/cards/ZombieCard.java
+++ src/main/java/com/noteasyok/spcialsmp/cards/ZombieCard.java
@@ -8,6 +8,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.Material;
 import org.bukkit.entity.Zombie;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityTargetEvent;
 import org.bukkit.inventory.EntityEquipment;
@@ -182,7 +183,6 @@ public class ZombieCard extends BaseCard implements Listener {
             e.setCancelled(true);
         }
     }
-}
 
     @EventHandler
     public void onMobTarget(EntityTargetEvent e) {
