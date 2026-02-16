dab3327c5712b14e0524eba7ee22d572c59b5491
NotEasyOk <masterplayz755@gmail.com>
2026-02-16 20:54:32 +0530

    Update CardRegistry.java

--- src/main/java/com/noteasyok/spcialsmp/manager/CardRegistry.java
+++ src/main/java/com/noteasyok/spcialsmp/manager/CardRegistry.java
@@ -23,6 +23,9 @@ public class CardRegistry {
         register(new HerobrineCard(), List.of("§7Left: Lightning", "§7Right: Darkness + Fly", "§7Shift+R: Giant/Tiny Mode"));
         register(new NothingCard(), List.of("§7Left: Time Set", "§7Right: Mind Control", "§7Shift+R: No Fall damage"));
         register(new WardenCard(), List.of("§7Left: Darkness", "§7Right: Sonic Boom", "§7Shift+R: Tank Mode"));
+        register(new IllusionistCard(), List.of("§7Left: Mind Fracture", "§7Right: Info", "§7Shift+R: 5 Shadow Clones"));
+        register(new ArchitectCard(), List.of("§7Left: Bedrock Prison", "§7Right: Info", "§7Shift+R: Sky Gold Bridge"));
+        register(new MirrorCard(), List.of("§7Left: Identity Thief", "§7Right: Info", "§7Shift+R: Damage Reflection"));
         register(new GravityCard(), List.of("§7Left: Gravity Yeet", "§7Right: Black Hole Vortex", "§7Shift+R: Zero Gravity Zone"));
         register(new CreeperCard(), List.of("§7Left: Explosion", "§7Right: Airstrike", "§7Shift+R: Nuke Rain"));
         register(new LightingCard(), List.of("§7Left: Strike", "§7Right: Storm", "§7Shift+R: Trail"));
