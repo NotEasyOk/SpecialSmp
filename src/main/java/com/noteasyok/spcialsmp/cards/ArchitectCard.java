b17e4c4b3b9799603665390ac509be1be7acaa33
NotEasyOk <masterplayz755@gmail.com>
2026-02-17 11:30:05 +0530

    Update ArchitectCard.java

--- src/main/java/com/noteasyok/spcialsmp/cards/ArchitectCard.java
+++ src/main/java/com/noteasyok/spcialsmp/cards/ArchitectCard.java
@@ -20,7 +20,7 @@ public class ArchitectCard extends BaseCard {
     @Override
     public String getName() { return "Architect Card"; }
     @Override
-    public Material getMaterial() { return Material.GOLD_INGOT; }
+    public Material getMaterial() { return Material.MAGENTA_DYE; }
     @Override
     public int getModelData() { return 102; }
 
