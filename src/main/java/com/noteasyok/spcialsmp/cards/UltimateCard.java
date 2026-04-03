536e0bcbff3b744698ca413c62e9db4b2d2d21f5
NotEasyOk <masterplayz755@gmail.com>
2026-04-03 20:49:42 +0530

    Update UltimateCard.java

--- src/main/java/com/noteasyok/spcialsmp/cards/UltimateCard.java
+++ src/main/java/com/noteasyok/spcialsmp/cards/UltimateCard.java
@@ -218,7 +218,8 @@ if (timer % 15 == 0) {
                 center.getWorld().spawnParticle(Particle.LARGE_SMOKE, center, 400, 10, 8, 10, 0.05);
                 timer++;
             }
-        }.runTaskTimer(SpcialSmp.get(), 0L, 1L);
+        stormTask.runTaskTimer(SpcialSmp.get(), 0L, 1L);
+       activeStormTasks.put(p.getUniqueId(), stormTask);
     } 
 
     /* Baaki Right Click, Shift Right, aur helper methods same rakhein... */
